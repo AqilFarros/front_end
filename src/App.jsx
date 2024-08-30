ee40f19346ed17cd9f2cd4673cdeecf636c55329
AqilFarros <aqilghonim@gmail.com>
2024-08-30 20:35:30 +0700

    sedikit banget waktunya?

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Welcome />} />
+        <Route path='/blog' element={<Welcome />} />
       </Routes>
     </Router>
   );
